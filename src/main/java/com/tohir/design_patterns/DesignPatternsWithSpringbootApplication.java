05e95d9686a00b4dc1bfaecb2912b0c2fcd957e5
Tohir Raihan <tohir.raihan@gmail.com>
2024-01-10 00:00:59 +0600

    :pencil2: Initial Commit

---
+++ src/main/java/com/tohir/design_patterns/DesignPatternsWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tohir.design_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tohir/design_patterns/DesignPatternsWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tohir.design_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
